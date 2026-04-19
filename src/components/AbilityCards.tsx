c73a2c21fa701338b5fb5ddb1c9bb35458d21816
Anthony Maitz <anthony@forevrgames.com>
2026-04-19 11:59:36 -0700

    feat: wire DescriptionCards and DeathCard into AbilityCards

--- src/components/AbilityCards.tsx
+++ src/components/AbilityCards.tsx
@@ -1,12 +1,18 @@
-import { For, createMemo } from 'solid-js'
+import { For, createMemo, Show } from 'solid-js'
 import { AbilityCard } from './AbilityCard'
+import { DescriptionCard } from './DescriptionCard'
+import { DeathCard } from './DeathCard'
 import type { AbilityCard as AbilityCardType, CombatState } from '../types'
 
 interface AbilityCardsProps {
   abilities: AbilityCardType[]
+  descriptions: Record<string, string>
+  deathContent: string
   charClass: string
   profession: string
+  personality: string
   combat: CombatState
+  hp: number
   onRoll: (sides: number) => void
 }
 
@@ -25,6 +31,32 @@ export function AbilityCards(props: AbilityCardsProps) {
 
   return (
     <div style={{ padding: '12px 14px' }}>
+      {/* Death card: shown above all else when HP = 0 */}
+      <Show when={props.hp === 0 && props.deathContent}>
+        <DeathCard body={props.deathContent} />
+      </Show>
+
+      {/* Description cards: collapsed, at top of list */}
+      <Show when={props.descriptions[props.personality]}>
+        <DescriptionCard
+          title={props.personality}
+          body={props.descriptions[props.personality]}
+        />
+      </Show>
+      <Show when={props.descriptions[props.charClass]}>
+        <DescriptionCard
+          title={props.charClass}
+          body={props.descriptions[props.charClass]}
+        />
+      </Show>
+      <Show when={props.descriptions[props.profession]}>
+        <DescriptionCard
+          title={props.profession}
+          body={props.descriptions[props.profession]}
+        />
+      </Show>
+
+      {/* Ability cards */}
       <For each={visible()}>
         {(card) => <AbilityCard card={card} onRoll={props.onRoll} />}
       </For>
